142b717d4a5963aa0c90fb0fc8ae1cd5d1ec9a27
Nicholas <null_86@bk.ru>
2022-10-14 18:56:50 +0600

    Init

---
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel {
+    int hp;
+
+    public int GetHP() {
+        return hp;
+    }
+
+    public void SetHP(int value) {
+        hp = value;
+    }
+
+
+}
